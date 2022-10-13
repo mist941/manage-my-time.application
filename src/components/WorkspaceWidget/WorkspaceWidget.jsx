f852a1d54d818b5e2fd0be94862445456400f5d8
ivanstatkevich <statkevich.ivan999@gmail.com>
2022-10-13 12:56:07 +0300

    fixed bugs with date filter

--- src/components/WorkspaceWidget/WorkspaceWidget.jsx
+++ src/components/WorkspaceWidget/WorkspaceWidget.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View} from 'react-native';
 
-const WorkspaceWidget = () => {
+const WorkspaceWidget = ({tasks}) => {
   return (
     <View>
 
--- src/screens/DashboardScreen/DashboardScreen.jsx
+++ src/screens/DashboardScreen/DashboardScreen.jsx
@@ -5,35 +5,41 @@ import DayPicker from '../../components/DayPicker/DayPicker';
 import PageHeader from '../../components/PageHeader/PageHeader';
 import services from '../../services';
 import Preloader from '../../components/Preloader/Preloader';
+import WorkspaceWidget from '../../components/WorkspaceWidget/WorkspaceWidget';
 
-const DashboardScreen = () => {
+const DashboardScreen = ({navigation}) => {
   const [selectedDate, setSelectDate] = useState(new Date());
   const [loading, setLoading] = useState(true);
   const [tasks, setTasks] = useState([]);
 
-  useEffect(() => {
+  const load = () => {
     const queryParams = {
-      start_date: selectedDate
+      start_date: selectedDate,
     };
-
     services.tasksServices.getTasks(queryParams).then(res => {
       if (res.data) {
         setTasks(res.data);
         setLoading(false);
       }
-    })
-  }, []);
+    });
+  }
 
+  useEffect(() => {
+    return navigation.addListener('focus', () => load());
+  }, [navigation]);
+
+  useEffect(() => load(), [selectedDate]);
 
   if (loading) return <Preloader/>;
 
   return (
     <AppLayout>
       <PageHeader name="Dashboard"/>
+      <WorkspaceWidget tasks={tasks}/>
       <DayPicker
         styles={TaskFormStyles.fromField}
         value={selectedDate}
-        onChange={setSelectDate}
+        onChange={date => setSelectDate(new Date(date))}
       />
     </AppLayout>
   );
