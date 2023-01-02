cc17771f8a325f6f62cdd4fd4cf143632d5e1638
ivanstatkevich <statkevich.ivan999@gmail.com>
2023-01-02 09:55:13 +0200

    delete language select

--- src/screens/LoginScreen/LoginScreen.jsx
+++ src/screens/LoginScreen/LoginScreen.jsx
@@ -3,14 +3,12 @@ import {LoginScreenStyles} from './LoginScreen.styles';
 import React from 'react';
 import LoginBanner from './components/LoginBanner/LoginBanner';
 import LoginButtonsGroup from './components/LoginButtonsGroup/LoginButtonsGroup';
-import LanguageButtonsGroup from './components/LanguageButtonsGroup/LanguageButtonsGroup';
 
 const LoginScreen = () => {
   return (
     <View style={LoginScreenStyles.wrapper}>
       <LoginBanner/>
       <LoginButtonsGroup/>
-      <LanguageButtonsGroup/>
     </View>
   );
 };
